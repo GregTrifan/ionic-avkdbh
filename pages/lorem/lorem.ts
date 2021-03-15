e89fcea638e19eeec664499c68f96774975e647f
TheSlayer-666 <theslayer69@protonmail.com>
2021-03-15 12:53:14 +0200

    Initial commit

---
+++ pages/lorem/lorem.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "page-lorem",
+  templateUrl: "lorem.html"
+})
+export class LoremPage {
+  constructor(public navCtrl: NavController) {}
+}
